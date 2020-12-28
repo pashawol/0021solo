e7c3064dae57a936ace476b8351c5c07b606926b
luckyone <luckyone1221@gmail.com>
2020-12-28 04:21:26 +0200

    begin 05

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -196,7 +196,7 @@ function eventHandler() {
 
 	var x = window.location.host;
 	let screenName;
-	screenName = 'main375.png';
+	screenName = '05.png';
 	if (screenName && x === "localhost:3000") {
 		$(".footer").after(`<div class="pixel-perfect" style="background-image: url(screen/${screenName});"></div>`);
 	}
@@ -581,6 +581,39 @@ function eventHandler() {
 	});
 	$('.close-sm-menu-js').click(closeSubMenuSM);
 
+
+	//page 05
+	$('.q-head-js').click(function (){
+		$(this).toggleClass('active');
+		$(this.parentElement).find('.q-descr-js').slideToggle(function (){
+			$(this).toggleClass('active');
+		});
+	});
+
+	//img-svg
+	$('img.img-svg-js').each(function () {
+		var $img = $(this);
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+		$.get(imgURL, function (data) {
+			// Get the SVG tag, ignore the rest
+			var $svg = $(data).find('svg'); // Add replaced image's classes to the new SVG
+
+			if (typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass + ' replaced-svg');
+			} // Remove any invalid XML tags as per http://validator.w3.org
+
+
+			$svg = $svg.removeAttr('xmlns:a'); // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+
+			if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+				$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
+			} // Replace image with new SVG
+
+
+			$img.replaceWith($svg);
+		}, 'xml');
+	});
 
 
 };
