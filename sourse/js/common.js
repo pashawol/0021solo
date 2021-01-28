474ea9d06ef4ac8bad4cbae75b94c83a9cf5d876
luckyone <luckyone1221@gmail.com>
2021-01-28 22:17:04 +0200

    made sliders fixes

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -600,7 +600,6 @@ function eventHandler() {
 	//img-svg
 	$('img.img-svg-js').each(function () {
 		var $img = $(this);
-		console.log(this);
 		var imgClass = $img.attr('class');
 		var imgURL = $img.attr('src');
 		$.get(imgURL, function (data) {
