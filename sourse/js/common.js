525b645b284b0fd2e6c592ed0b453e9554b2c757
Pavel <wol1414@gmail.com>
2021-04-27 15:24:34 +0300

    remove svg attr

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -629,7 +629,8 @@ function eventHandler() {
 			} // Remove any invalid XML tags as per http://validator.w3.org
 
 
-			$svg = $svg.removeAttr('xmlns:a'); // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+			$svg = $svg.removeAttr('xmlns:a').style.removeProperty('enable-background'); // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+			// $svg = $svg.removeAttr('xmlns:a'); // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
 
 			if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
 				$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
